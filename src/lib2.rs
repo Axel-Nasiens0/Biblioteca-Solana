a2e2ae5bb7a3b5b89062a413dd73b54f3b1a6d97
Axel-Nasiens0 <25190843@utcalvillo.edu.mx>
2026-02-27 14:15:46 -0600

    Update lib2.rs

--- src/lib2.rs
+++ src/lib2.rs
@@ -1,13 +1,41 @@
 use anchor_lang::prelude::*; //Se utiliza el 100% de todos los casos
 
-declare_id!("");
+declare_id!("5bVdMK7qMACeJiAjTxXpgYahdve6YFeR5GdBFNPHVNFd");
 
-#[programa] //Significa que el código va a ser usado en el programa
+#[program] //Significa que el código va a ser usado en el programa
 pub mod biblioteca{
     use super::*; //Exportar todo el código
 
-    pub fn crear_biblioteca() -> Result<()>{
-        //Código...
+    pub fn crear_biblioteca(context: Context<NuevaBiblioteca>, nombre: String) -> Result<()>{
+        let owner_id = context.accounts.owner.key();
+        let libros: Vec<Libros> = Vec::new();
+
+        context.accounts.biblioteca.set_inner(Biblioteca {
+            owner: owner_id,
+            nombre,
+            libros,
+        });
+
+        Ok(())
+    }
+
+    pub fn agregar_libro(context: Context<NuevoLibro>, nombre: String, paginas: u16) -> Result<()>{
+        let libro= Libros{
+            nombre: nombre,
+            paginas,
+            disponible: true,
+        };
+
+        context.accounts.biblioteca.libros.push(libro);
+
+        Ok(())
+    }
+
+    pub fn ver_libros(context: Context<NuevoLibro>) -> Result<()>{
+        let libros = context.accounts.biblioteca.libros;
+        msg!("La lista de libros es: {:#?}", libros);
+
+        Ok(())
     }
 }
 
@@ -20,11 +48,11 @@ pub struct Biblioteca{
     nombre: String,
 
     #[max_len(10)]
-    libro: Vec<Libro>,
+    libros: Vec<Libros>,
 } 
 
-#[derive(AnchorSeriolize, AnchorDeseriolize, Clone, InitSpace, PartialEq, Debug)]
-pub struct Libro {
+#[derive(AnchorSerialize, AnchorDeseriolize, Clone, InitSpace, PartialEq, Debug)]
+pub struct Libros {
     #[max_len(60)]
     nombre: String,
 
@@ -34,7 +62,7 @@ pub struct Libro {
 }
 
 #[derive(Account)]
-pub struct NuevaBiblioteca {
+pub struct NuevaBiblioteca<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
 
@@ -50,9 +78,10 @@ pub struct NuevaBiblioteca {
     pub system_program: Program<'info, System>,
 }
 
-pub struct NuevoLibro {
-    pub owner: Signer<'info>,
+#[derive(Accounts)] 
+pub struct NuevoLibro<'info> {
+    pub owner: Signer<'info>, 
+    #[account(mut)] 
+    pub biblioteca: Account<'info, Biblioteca>, 
 
-    #[account(mut)]
-    pub biblioteca: Account<'info, Biblioteca>,
 }
